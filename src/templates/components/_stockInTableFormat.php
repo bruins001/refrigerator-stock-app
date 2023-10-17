fee18be7fe80bb1eaea28fc3ec5604cd50b9dcd3
Niels Bruinsma <niels.bruinsma2002@gmail.com>
2023-10-17 10:45:51 +0200

    Update _stockInTableFormat.php added image and sample data

--- src/templates/components/_stockInTableFormat.php
+++ src/templates/components/_stockInTableFormat.php
@@ -3,7 +3,42 @@
         <th>Photo</th>
         <th>Name</th>
         <th>Type</th>
-        <th>Date</th>
-        <th>EAN</th>
+        <th>Expiration date</th>
+        <th>Barcode</th>
+    </tr>
+    <tr>
+        <th><img src="/assets/images/placeholder.jpg"></img></th>
+        <th>Placeholder name</th>
+        <th>Placeholder type of food</th>
+        <th>1-1-2024</th>
+        <th>123456789123</th>
+    </tr>
+    <tr>
+        <th><img src="/assets/images/placeholder.jpg"></img></th>
+        <th>Placeholder name</th>
+        <th>Placeholder type of food</th>
+        <th>1-1-2024</th>
+        <th>123456789123</th>
+    </tr>
+    <tr>
+        <th><img src="/assets/images/placeholder.jpg"></img></th>
+        <th>Placeholder name</th>
+        <th>Placeholder type of food</th>
+        <th>1-1-2024</th>
+        <th>123456789123</th>
+    </tr>
+    <tr>
+        <th><img src="/assets/images/placeholder.jpg"></img></th>
+        <th>Placeholder name</th>
+        <th>Placeholder type of food</th>
+        <th>1-1-2024</th>
+        <th>123456789123</th>
+    </tr>
+    <tr>
+        <th><img src="/assets/images/placeholder.jpg"></img></th>
+        <th>Placeholder name</th>
+        <th>Placeholder type of food</th>
+        <th>1-1-2024</th>
+        <th>123456789123</th>
     </tr>
 </table>
