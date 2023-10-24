4833c5d39050a33c855c8b501187b6f42f52038f
bruins001 <niels.bruinsma2002@gmail.com>
2023-10-24 16:43:16 +0200

    Update app.js and _stockInTableFormat.php
    - Add support for adding rows via JavaScript.
    - Table rows now in tbody tags.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -35,8 +35,32 @@ class Table {
             this.columnsArray.push(newColumn);
         }
     }
+
+    addRecord = (newRecordArray) => {
+        if (newRecordArray.length != this.columnsArray.length) {
+            console.log(this.columnsArray.length)
+            throw new Error('Argument "newRecordArray" is not the same length as the number of colums in this Table.');
+        }
+
+        let domNewTableRow = document.createElement('tr');
+        for (let idx=0; idx<newRecordArray.length; idx++) {
+            let domNewRecord = document.createElement('td');
+            domNewRecord.append(newRecordArray[idx]);
+            domNewTableRow.append(domNewRecord);
+            this.columnsArray[idx].domRowArray.push(domNewRecord);
+        }
+
+        this.table.getElementsByTagName('tbody')[0].appendChild(domNewTableRow);
+        this.domRecordsArray.push(domNewTableRow);
+    }
 }
 
 let domTable = document.getElementById('table-in-stock');
 const table = new Table(domTable);
+const domNewImgElement = document.createElement('img');
+domNewImgElement.src = '/assets/images/placeholder.jpg';
+const newRecordArray = [domNewImgElement, 'placeholder', 'placeholder', 'placeholder', 'placeholder'];
+table.addRecord(newRecordArray);
+
 console.log(table.columnsArray);
+console.log(table.domRecordsArray);
--- src/templates/demo/components/_stockInTableFormat.php
+++ src/templates/demo/components/_stockInTableFormat.php
@@ -1,9 +1,11 @@
-<table id="table-in-stock">
-    <tr>
-        <th>Photo</th>
-        <th>Name</th>
-        <th>Type</th>
-        <th>Expiration date</th>
-        <th>Barcode</th>
-    </tr>
+<table id="table-in-stock">`
+    <tbody>
+        <tr>
+            <th>Photo</th>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Expiration date</th>
+            <th>Barcode</th>
+        </tr>
+    </tbody>
 </table>
