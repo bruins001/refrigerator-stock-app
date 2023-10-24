5ee68096a5f46f39187932ae31508bb72df6ea11
bruins001 <niels.bruinsma2002@gmail.com>
2023-10-24 12:12:16 +0200

    Change _stockInTableFormat.php in demo and default
    Remove placeholder table data.

--- src/templates/default/components/_stockInTableFormat.php
+++ src/templates/default/components/_stockInTableFormat.php
@@ -6,43 +6,4 @@
         <th>Expiration date</th>
         <th>Barcode</th>
     </tr>
-    <?php
-    if (isset($_SESSION["user"])) {
-        echo '<tr>
-                <th><img src="/assets/images/placeholder.jpg"></img></th>
-                <th>Placeholder name</th>
-                <th>Placeholder type of food</th>
-                <th>1-1-2024</th>
-                <th>123456789123</th>
-            </tr>
-            <tr>
-                <th><img src="/assets/images/placeholder.jpg"></img></th>
-                <th>Placeholder name</th>
-                <th>Placeholder type of food</th>
-                <th>1-1-2024</th>
-                <th>123456789123</th>
-            </tr>
-            <tr>
-                <th><img src="/assets/images/placeholder.jpg"></img></th>
-                <th>Placeholder name</th>
-                <th>Placeholder type of food</th>
-                <th>1-1-2024</th>
-                <th>123456789123</th>
-            </tr>
-            <tr>
-                <th><img src="/assets/images/placeholder.jpg"></img></th>
-                <th>Placeholder name</th>
-                <th>Placeholder type of food</th>
-                <th>1-1-2024</th>
-                <th>123456789123</th>
-            </tr>
-            <tr>
-                <th><img src="/assets/images/placeholder.jpg"></img></th>
-                <th>Placeholder name</th>
-                <th>Placeholder type of food</th>
-                <th>1-1-2024</th>
-                <th>123456789123</th>
-            </tr>';
-    }
-    ?>
 </table>
