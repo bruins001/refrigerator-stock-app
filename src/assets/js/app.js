05c38a98315ad6d63c7bfc174f62524bc5365442
bruins001 <niels.bruinsma2002@gmail.com>
2023-10-24 12:02:49 +0200

    Rewrite Table and Column Class

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,29 +1,34 @@
-class Table {
-    constructor(domTable) {
-        this.table = domTable;
-        this.domAllColumnsArray = Array.from(this.table.getElementsByTagName('tr'));
-    }
-}
-
 class Column {
     constructor(tableOfColoum, columnName) {
         this.tableOfColoum = tableOfColoum;
-        this.columnName = columnName;
+        this.domColumnHeader = HTMLElement;
         this.domRowArray = new Array();
         
         let coloumIndex = -1;
-        let tableHeadersArray = this.tableOfColoum.domAllColumnsArray[0].getElementsByTagName('th');
+        let tableHeadersArray = this.tableOfColoum.headers;
         for (let idx=0; idx < tableHeadersArray.length; idx++) {
             coloumIndex++;
-            if (tableHeadersArray[idx].innerHTML === this.columnName) {
+            let domColumnHeader = tableHeadersArray[idx];
+            if (domColumnHeader.innerHTML === columnName) {
+                this.domColumnHeader = domColumnHeader;
                 break;
             }
         }
 
-        for (let idx=1; idx < this.tableOfColoum.domAllColumnsArray.length; idx++) {
-            let domRow = this.tableOfColoum.domAllColumnsArray[idx];
+        for (let idx=0; idx < this.tableOfColoum.domRecordsArray.length; idx++) {
+            let domRow = this.tableOfColoum.domRecordsArray[idx];
             this.domRowArray.push(domRow.getElementsByTagName('th')[coloumIndex]);
         }
+    }
+}
+
+class Table {
+    constructor(domTable) {
+        this.table = domTable;
+        let domAllColumnsArray = Array.from(this.table.getElementsByTagName('tr'));
+        this.headers = domAllColumnsArray[0].getElementsByTagName('th');
+        domAllColumnsArray.shift();
+        this.domRecordsArray = domAllColumnsArray;
     }
 }
 
