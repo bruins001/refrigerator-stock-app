e5feb1592aeb62aecdfbde08ffc6c147464bd1c9
bruins001 <62013574+bruins001@users.noreply.github.com>
2023-10-25 09:35:04 +0000

    Update homepage.php in default and demo templates

--- src/templates/demo/homepage.php
+++ src/templates/demo/homepage.php
@@ -7,9 +7,30 @@
     <title>Homepage</title>
 </head>
 <body>
-    <?php include 'components/_header.php'; ?>
+  <?php include 'components/_header.php'; ?>
+  <main>
     <h1>Refrigerator stock app</h1>
-    <?php include 'components/_stockInTableFormat.php'; ?>
-    <script src="/assets/js/app.js"></script>
+    <h2>Product form</h2>
+    <form id="add-product-form">
+      <label for="photo">Photo: </label>
+      <input type="file" name="photo"></input>
+      <br />
+      <label for="name">Name: </label>
+      <input type="text" name="name" required></input>
+      <br />
+      <label for="type">Type: </label>
+      <input type="text" name="type" required></input>
+      <br />
+      <label for="experation-date">Experation date: </label>
+      <input type="text" for="experation-date" required></input>
+      <br />
+      <label type="text" for="barcode">Barcode: </label>
+      <input type="number" for="barcode" min="12" max="12" required></input>
+      <br />
+      <input type="submit">
+    </form>
+  </main>
+  <?php include 'components/_stockInTableFormat.php'; ?>
+  <script src="/assets/js/app.js"></script>
 </body>
 </html>
